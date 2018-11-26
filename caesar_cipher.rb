335a8e58285946e4b4796b820ebad1369bd97bce
Trillian <trillianjosea@gmail.com>
2018-11-26 15:08:15 +0100

    fixing

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,17 +3,17 @@ def caesar_cipher(text, shift)
   downcase_letter = ('a'..'z').to_a
   upcase_letter = ('A'..'Z').to_a
 
-	text.split('').each do |character|
-		if  downcase_letter.include?(character)
-			coded <<  downcase_letter[( downcase_letter.index(character) + shift ) % 26]
-		elsif upcase_letter.include?(character)
-			coded << upcase_letter[( upcase_letter.index(character) + shift ) % 26]
-		else
-			coded << character
-		end
-	end
+  text.split('').each do |character|
+    if  downcase_letter.include?(character)
+      coded <<  downcase_letter[( downcase_letter.index(character) + shift ) % 26]
+    elsif upcase_letter.include?(character)
+      coded << upcase_letter[( upcase_letter.index(character) + shift ) % 26]
+    else
+      coded << character
+    end
+  end
 
-	coded.join
+    coded.join
 end
 
 puts 'Write the word to encript'
